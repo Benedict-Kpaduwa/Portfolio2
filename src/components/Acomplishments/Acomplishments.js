cbdf3abfce921d5a607f7696132f005a27007bb9
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2022-01-21 04:36:17 +0100

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,7 +4,7 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 10, text: 'Open Source Projects'},
+  { number: 20, text: 'Open Source Projects'},
 ];
 
 const Acomplishments = () => (
